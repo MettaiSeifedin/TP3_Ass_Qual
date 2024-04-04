740493cbfc3619143d3628db13ea142e64eab50f
seifo <mettaiseifedin@gmail.com>
2024-04-04 01:54:41 -0700

    1

---
+++ src/test/java/Exo2/UserServiceTest.java
@@ -0,0 +1,38 @@
+package Exo2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.example.Exo2.*;
+import static org.mockito.Mockito.*;
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+    @Mock
+    private UtilisateurApi utilisateurApiMock;
+    @Mock
+    private UserService userService ;
+    @Test
+    public void testCreerUtilisateur()throws ServiceException {
+// Création d'un nouvel utilisateur
+        Utilisateur utilisateur = new Utilisateur("Jean", "Dupont", "jeandupont@email.com");
+// TODO : Configuration du comportement du mock, utiliser la
+//directive « when » avec sa méthode « thenReturn »
+        doNothing().when(utilisateurApiMock).creerUtilisateur(utilisateur);
+// ...
+// TODO : Création du service avec le mock
+        UserService userService = new UserService(utilisateurApiMock);
+// ...
+// TODO : Appel de la méthode à tester
+        try {
+            userService.creerUtilisateur(utilisateur);
+        } catch (ServiceException e) {
+            // Gérer l'exception si nécessaire
+
+        }
+// ...
+// TODO : Vérification de l'appel à l'API
+// ...
+        verify(utilisateurApiMock).creerUtilisateur(utilisateur);
+    }
+}
