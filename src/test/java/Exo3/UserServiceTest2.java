0ae6278aa780302deb95320ffba21764fe0d7106
seifo <mettaiseifedin@gmail.com>
2024-04-15 02:54:46 -0700

    exo3

---
+++ src/test/java/Exo3/UserServiceTest2.java
@@ -0,0 +1,46 @@
+package Exo3;
+
+import org.example.Exo2.ServiceException;
+import org.example.Exo2.UserService;
+import org.example.Exo2.Utilisateur;
+import org.example.Exo2.UtilisateurApi;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest2 {
+    @Mock
+    private UtilisateurApi utilisateurApiMock;
+    @Mock
+    private UserService userService ;
+    @Test
+    public void testCreerUtilisateur()throws ServiceException {
+// Création d'un nouvel utilisateur
+        Utilisateur utilisateur = new Utilisateur("Jean", "Dupont", "jeandupont@email.com");
+// TODO : Configuration du comportement du mock, utiliser la
+//directive « when » avec sa méthode « thenReturn »
+
+       doThrow(new ServiceException("Echec de la création de l'utilisateur")).when(utilisateurApiMock).creerUtilisateur(utilisateur);
+// ...
+// TODO : Création du service avec le mock
+        UserService userService = new UserService(utilisateurApiMock);
+// ...
+// TODO : Appel de la méthode à tester
+        try {
+            userService.creerUtilisateur(utilisateur);
+        } catch (ServiceException e) {
+            // Gérer l'exception si nécessaire
+
+
+        }
+// ...
+// TODO : Vérification de l'appel à l'API
+// ...
+        verify(utilisateurApiMock).creerUtilisateur(utilisateur);
+    }
+}
